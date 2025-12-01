2523241bb1cc2fdad790a55d6433ea2af0877bf6
Jan Lubojacky <jan.lubojacky@gmail.com>
2025-12-01 22:37:09 +0100

    day1 second part

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,18 +2,24 @@ use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+// For the first exercise I had 1007 which is correct
+// before I had something that was too low
+// 5829 TOO high
+// 6219 TOO high
+
 struct SafeDial {
-    rotation: i32,
-    positions: i32,
+    position: i32,
+    number_of_positions: i32,
 }
 
 /// rotations left and right with wrapping the result bettween min and max
 impl SafeDial {
-    fn left(&mut self, by: usize) {
-        self.rotation = (self.rotation - by as i32).rem_euclid(self.positions);
+    fn left(&mut self, by: i32) {
+        self.position = (self.position - by).rem_euclid(self.number_of_positions)
     }
-    fn right(&mut self, by: usize) {
-        self.rotation = (self.rotation + by as i32).rem_euclid(self.positions);
+
+    fn right(&mut self, by: i32) {
+        self.position = (self.position + by).rem_euclid(self.number_of_positions)
     }
 }
 
@@ -30,13 +36,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let reader = BufReader::new(file);
 
     let mut dial = SafeDial {
-        rotation: 50,
-        positions: 100,
+        position: 50,
+        number_of_positions: 100,
     };
 
     let mut password = 0;
 
-    // println!("Dial starts at {}", dial.rotation);
+    println!("Dial starts at {}", dial.position);
 
     // execute the rotation while keeping the state
     for line in reader.lines() {
@@ -47,21 +53,47 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .next()
             .ok_or("Line is empty, expected direction character")?;
         let number_string: String = chars.collect();
-        let amount = number_string
-            .parse::<usize>()
+        let mut amount = number_string
+            .parse::<i32>()
             .map_err(|e| format!("Failed to parse amount '{}': {}", number_string, e))?;
 
-        match direction {
-            'L' => dial.left(amount),
-            'R' => dial.right(amount),
-            _ => return Err(format!("Unexpected direction '{}'", direction).into()),
-        }
-
-        if dial.rotation == 0 {
+        // handle cases when number of rotations is > the number of positions
+        while amount >= dial.number_of_positions {
+            amount -= dial.number_of_positions;
+            println!("huge rotation +1");
             password += 1;
         }
 
-        // println!("Dial is rotated {}, points at {}", line, dial.rotation)
+        let prev_position = dial.position;
+
+        // rotation to 0 from right couns twice
+
+        match direction {
+            'L' => {
+                dial.left(amount);
+                if (dial.position > prev_position) && (prev_position != 0) && (dial.position != 0) {
+                    // we decreased the value so if we have a bigger value then before we had to
+                    // pass trough zero, starting from 0 doesn't count as a pass
+                    println!("PASS 0 BY LEFT ROT");
+                    password += 1;
+                }
+            }
+            'R' => {
+                dial.right(amount);
+                if (dial.position < prev_position) && (prev_position != 0) && (dial.position != 0) {
+                    println!("PASS 0 BY RIGHT ROT");
+                    password += 1;
+                }
+            }
+            _ => return Err(format!("Unexpected direction '{}'", direction).into()),
+        }
+
+        if dial.position == 0 {
+            println!("DIAL ENDED UP AT 0");
+            password += 1;
+        }
+
+        println!("Dial is rotated {}, points at {}", line, dial.position)
     }
 
     println!("The password is {}", password);
