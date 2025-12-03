b30c7cf9f67e5b5ee6458449d91c579eab708806
Jan Lubojacky <jan.lubojacky@gmail.com>
2025-12-03 13:34:41 +0100

    day3 part2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,5 +1,3 @@
-/// This is a brute force solution, it would be more elegant to generate the invalid ids in each
-/// range instead
 use clap::Parser;
 use std::cmp::PartialOrd;
 use std::error::Error;
@@ -13,23 +11,9 @@ use std::{
 struct Args {
     #[arg(short, long)]
     input: String,
+    #[arg(short, long)]
+    limit: u64,
 }
-
-// This would not work anyways, default probably doesn't make sense? Would make more sense to
-// return None if the slice is empty and set max_val to the first element if it is not but at that
-// point it writing it this way with the forloop seems less ergonomic than the max_by with custom
-// ordering
-// fn max_idx_and_val_from_slice<T: PartialOrd + Default + Copy>(arr: &[T]) -> (usize, T) {
-//     let (mut max_idx, mut max_val): (usize, T) = (0, T::default());
-//     for (idx, val) in arr.iter().enumerate() {
-//         if *val > max_val {
-//             max_idx = idx;
-//             max_val = *val;
-//         }
-//     }
-//
-//     return (max_idx, max_val);
-// }
 
 fn max_idx_and_val_from_slice<T: PartialOrd + Clone>(arr: &[T]) -> Option<(usize, T)> {
     arr.iter()
@@ -47,35 +31,42 @@ fn max_idx_and_val_from_slice<T: PartialOrd + Clone>(arr: &[T]) -> Option<(usize
 /// battery_bank is the array of batteries
 /// power_limitter is the number of batteries that should be turned on
 /// returns the maximum power this array can give given the limitter
-fn battery_tuner(battery_bank: Vec<u32>, power_limitter: u32) -> u32 {
-    let (first_idx, first_value) =
-        max_idx_and_val_from_slice(&battery_bank[0..battery_bank.len() - 1])
+fn battery_tuner(battery_bank: &[u64], power_limit: u64) -> String {
+    if power_limit == 0 {
+        return String::new();
+    }
+    let remaining_limit = power_limit - 1;
+
+    let (max_idx, max_value) =
+        max_idx_and_val_from_slice(&battery_bank[0..battery_bank.len() - remaining_limit as usize])
             .expect("Bank is empty");
 
-    let (_, second_value) = max_idx_and_val_from_slice(&battery_bank[first_idx + 1..])
-        .expect("First index should not be last index in the array");
-
-    let result: u32 = (first_value.to_string() + &second_value.to_string())
-        .parse()
-        .expect("This should work");
-
-    result
+    return max_value.to_string()
+        + &battery_tuner(&battery_bank[max_idx + 1..], remaining_limit).to_string();
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     let file_path = args.input;
+    let power_limit = args.limit;
+
+    let mut total_power = 0;
+
     let file =
         File::open(&file_path).map_err(|e| format!("Failed to open file {}: {}", file_path, e))?;
     let reader = BufReader::new(file);
 
-    let mut total_power = 0;
     for line in reader.lines() {
         let line = line?;
 
-        let battery_bank: Vec<u32> = line.chars().filter_map(|ch| ch.to_digit(10)).collect();
+        let battery_bank: Vec<u64> = line
+            .chars()
+            .filter_map(|ch| ch.to_digit(10))
+            .map(|num| num as u64)
+            .collect();
 
-        total_power += battery_tuner(battery_bank);
+        let power: u64 = battery_tuner(&battery_bank, power_limit).parse()?;
+        total_power += power;
     }
 
     println!("total power is {total_power}");
