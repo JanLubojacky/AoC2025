3f269843ab1db893baf59b1a816ec1f4a0c7fdef
Jan Lubojacky <jenda.lubojacky@gmail.com>
2025-12-06 17:31:00 +0100

    separat part1 part2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -9,16 +9,15 @@ use std::{
 struct Args {
     #[arg(short, long)]
     input: String,
+    part: u8,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Args::parse();
-    let file_path = args.input;
-
+fn part1(file_path: String) -> Result<(), Box<dyn std::error::Error>> {
     let file =
         File::open(&file_path).map_err(|e| format!("Failed to open file {}: {}", file_path, e))?;
     let reader = BufReader::new(file);
     let lines: Vec<String> = reader.lines().collect::<Result<_, _>>()?;
+
     let mut rev_lines = lines.iter().rev();
 
     let signs = rev_lines.next().ok_or("File is empty")?;
@@ -39,24 +38,41 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .split_whitespace()
             .filter_map(|num| num.parse().ok())
             .collect();
-        for ((ref mut num, &num_next), &sign) in numbers
+        for ((num, &num_next), &sign) in numbers
             .iter_mut()
             .zip(next_numbers.iter())
             .zip(signs.iter())
         {
-            println!("{num} {sign} {num_next}");
-            let new_num = match sign {
-                "*" => &mut (**num * num_next),
-                "+" => &mut (**num + num_next),
+            // println!("{num} {sign} {num_next}");
+            *num = match sign {
+                "*" => *num * num_next,
+                "+" => *num + num_next,
                 _ => panic!("Illegal operation"),
             };
-            *num = new_num;
         }
     }
 
     let result: i64 = numbers.iter().sum();
 
     println!("part1 {result}");
+
+    Ok(())
+}
+
+fn part2(file_path: String) -> Result<(), Box<dyn std::error::Error>> {
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+    let file_path = args.input;
+    let part = args.part;
+
+    if part == 1 {
+        part1(file_path);
+    } else if part == 2 {
+        part2(file_path);
+    }
 
     Ok(())
 }
