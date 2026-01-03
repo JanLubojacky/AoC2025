34adf75799258e019b159c6cf1980cb833502658
Jan Lubojacky <jenda.lubojacky@gmail.com>
2026-01-03 16:26:11 +0100

    this doesn't work because cycles

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -15,35 +15,6 @@ struct Args {
     input: String,
 }
 
-/// graph should be a DAG so a bfs should finish eventually even without visited
-/// keep visited count for each vertex
-fn find_all_paths(
-    adj_list: HashMap<String, Vec<String>>,
-) -> Result<u64, Box<dyn std::error::Error>> {
-    let mut visited: HashMap<String, u64> = HashMap::new();
-    visited.insert("you".to_string(), 1);
-
-    let mut queue: VecDeque<String> = VecDeque::new();
-    queue.push_back("you".to_string());
-
-    // while queue not empty
-    while let Some(vertex) = queue.pop_front() {
-        *visited.entry(vertex.clone()).or_insert(0) += 1;
-        // println!("Vertex {vertex}");
-
-        // if this vertex has neighbours push them to queue
-        if let Some(neighbours) = adj_list.get(&vertex) {
-            for n in neighbours {
-                queue.push_back(n.clone());
-            }
-        }
-    }
-
-    let result = visited.get("out").ok_or("Out unreachable")?;
-
-    Ok(*result)
-}
-
 fn visited_counts(
     adj_list: &HashMap<String, Vec<String>>,
     queue: &mut VecDeque<String>,
@@ -52,9 +23,7 @@ fn visited_counts(
 
     while let Some(vertex) = queue.pop_front() {
         *visited.entry(vertex.clone()).or_insert(0) += 1;
-        // println!("Vertex {vertex}");
 
-        // if this vertex has neighbours push them to queue
         if let Some(neighbours) = adj_list.get(&vertex) {
             for n in neighbours {
                 queue.push_back(n.clone());
@@ -63,6 +32,21 @@ fn visited_counts(
     }
 
     visited
+}
+
+/// graph should be a DAG so a bfs should finish eventually even without visited
+/// keep visited count for each vertex
+fn find_all_paths(
+    adj_list: &HashMap<String, Vec<String>>,
+) -> Result<u64, Box<dyn std::error::Error>> {
+    let mut queue: VecDeque<String> = VecDeque::new();
+    queue.push_back("svr".to_string());
+
+    let visited_counts = visited_counts(&adj_list, &mut queue);
+
+    let result = visited_counts.get("out").ok_or("Out unreachable")?;
+
+    Ok(*result)
 }
 
 /// first part
@@ -76,9 +60,67 @@ fn visited_counts(
 ///
 /// reset everything again and run the first part from fft to out and dac to out
 fn find_all_dac_fft_paths(
-    adj_list: HashMap<String, Vec<String>>,
+    adj_list: &HashMap<String, Vec<String>>,
 ) -> Result<u64, Box<dyn std::error::Error>> {
-    Ok(0)
+    let mut queue: VecDeque<String> = VecDeque::new();
+    queue.push_back("svr".to_string());
+
+    let vc = visited_counts(adj_list, &mut queue);
+
+    let result = vc.get("out").ok_or("Out unreachable")?;
+
+    // let srv_to_fft = match vc.get("fft") {
+    //     Some(v) => *v,
+    //     None => 0,
+    // };
+    // let srv_to_dac = match vc.get("dac") {
+    //     Some(v) => *v,
+    //     None => 0,
+    // };
+    //
+    // println!("srv_to_fft: {srv_to_fft}");
+    // println!("srv_to_dac: {srv_to_dac}");
+    //
+    // queue = VecDeque::new();
+    // queue.push_back("fft".to_string());
+    // let vc = visited_counts(adj_list, &mut queue);
+    // let fft_to_dac = match vc.get("dac") {
+    //     Some(v) => *v,
+    //     None => 0,
+    // };
+    //
+    // println!("fft_to_dac: {fft_to_dac}");
+    // let fft_to_out = match vc.get("out") {
+    //     Some(v) => *v,
+    //     None => 0,
+    // };
+    //
+    // queue = VecDeque::new();
+    // queue.push_back("dac".to_string());
+    // let vc = visited_counts(adj_list, &mut queue);
+    // let dac_to_fft = match vc.get("fft") {
+    //     Some(v) => *v,
+    //     None => 0,
+    // };
+    //
+    // println!("dac_to_fft: {dac_to_fft}");
+    // let dac_to_out = match vc.get("out") {
+    //     Some(v) => *v,
+    //     None => 0,
+    // };
+    // println!("fft_to_out: {fft_to_out}");
+    // println!("dac_to_out: {dac_to_out}");
+    //
+    // let mut out = 0;
+    // if srv_to_dac != 0 && dac_to_fft != 0 && fft_to_out != 0 {
+    //     out += fft_to_out;
+    // }
+    // if srv_to_fft != 0 && fft_to_dac != 0 && dac_to_out != 0 {
+    //     out += dac_to_out;
+    // }
+    //
+    // Ok(out)
+    Ok(*result)
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -103,8 +145,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         adj_list.insert(vertex.to_string(), to_vertices);
     }
 
-    // let result = find_all_paths(adj_list)?;
-    // println!("PART 1: {result}");
+    let result = find_all_paths(&adj_list)?;
+    println!("PART 1: {result}");
+
+    // let result = find_all_dac_fft_paths(&adj_list)?;
+    // println!("PART 2: {result}");
 
     Ok(())
 }
